4e4754cb120cff4b254dc525fd1e167b1811724a
kylixor <93137659+kylixor@users.noreply.github.com>
2022-06-15 18:50:03 -0400

    Bar chart progress!

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# frontend
+# kyWeb
 
 ## Project setup
 ```
--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,46 @@
 <template>
-  <HelloWorld msg="Welcome to Your KyVue.js"/>
+	<div class="container">
+		<Bar v-if="loaded" :chart-data="chartData" />
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { Bar } from 'vue-chartjs'
+import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+	name: 'BarChart',
+	components: { Bar },
+	data: () => ({
+		loaded: false,
+		chartData: null
+	}),
+	async mounted() {
+		this.loaded = false
+
+		try {
+			const test = await fetch('http://localhost:3000')
+			console.log(test)
+			const { userlist } = await fetch('http://localhost:3000/fake-data')
+			console.log(userlist)
+			this.chartdata = userlist
+			this.chartData = {
+				labels: ['January', 'February', 'March'],
+				datasets: [
+					{
+						label: 'Data One',
+						backgroundColor: '#f87979',
+						data: [40, 30, 12]
+					}
+				]
+			}
+
+			this.loaded = true
+		} catch (e) {
+			console.error(e)
+		}
+	}
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
