3db39efca4076fa421d32d75db54b27a2ba4b887
kylixor <93137659+kylixor@users.noreply.github.com>
2022-06-27 22:38:46 -0400

    Parse Wordle data to graph correctly

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,63 @@
 <template>
 	<div class="container">
-		<Bar v-if="loaded" :chart-data="chartData" />
+		<Line v-if="loaded" :chart-data="chartData" :chart-options="chartOptions" />
 	</div>
 </template>
 
 <script>
-import { Bar } from 'vue-chartjs'
-import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+import { Line } from 'vue-chartjs'
+import {
+	Chart as ChartJS,
+	Title,
+	Tooltip,
+	Legend,
+	LineElement,
+	LinearScale,
+	PointElement,
+	CategoryScale,
+} from 'chart.js'
 
-ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+ChartJS.register(
+	Title,
+	Tooltip,
+	Legend,
+	LineElement,
+	LinearScale,
+	PointElement,
+	CategoryScale
+)
 
 export default {
-	name: 'BarChart',
-	components: { Bar },
+	name: 'LineChart',
+	components: { Line },
 	data: () => ({
 		loaded: false,
-		chartData: null
+		chartData: null,
+		chartOptions: {
+			responsive: true,
+			maintainAspectRatio: false,
+			spanGaps: true,
+		}
 	}),
 	async mounted() {
-		this.loaded = false
-
 		const userlist = await fetch('http://localhost:3000/fake-data')
 		const data = await userlist.json()
+
+		// Convert scores into a rolling average
+		var games_played = 0
+		var score_sum = 0
+		var average = NaN
+		for (var i = 0; i < data.datasets[0].data.length; i++) {
+			if (data.datasets[0].data[i] != 0) {
+				games_played++
+				score_sum += data.datasets[0].data[i]
+			}
+			if (games_played > 0) {
+				average = score_sum / games_played
+			}
+			data.datasets[0].data[i] = average
+		}
+
 		this.chartData = data
 		this.loaded = true
 	}
