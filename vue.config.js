b6b07ba7980c123edf7ccb4038779dd508fb477f
kylixor <93137659+kylixor@users.noreply.github.com>
2022-07-04 09:36:38 -0400

    Add dev server proxy to use /api

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
-const { defineConfig } = require("@vue/cli-service");
+const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
     transpileDependencies: true,
+    devServer: {
+        proxy: {
+            '^/api': {
+                target: 'https://localhost:3000',
+                changeOrigin: true,
+                logLevel: 'debug',
+                pathRewrite: { '^/api': '/' },
+            },
+        },
+    },
 });
