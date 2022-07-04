7c645c75b33e31de0ec1e51f486c29317df1b119
kylixor <93137659+kylixor@users.noreply.github.com>
2022-07-04 09:37:02 -0400

    Use vue-router to break site into views

---
+++ src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+    <LineGraph />
+</template>
+
+<script>
+import LineGraph from '../components/LineGraph.vue'
+
+export default {
+    name: 'HomePage',
+    components: {
+        LineGraph
+    },
+}
+</script>
